89619329b4d331447d7d587fc8719f8ec293a481
nicey0 <nicey_pm@protonmail.com>
2020-07-28 12:59:05 -0300

    label stays above when there's input + slower

--- Gmail/label_anim.js
+++ Gmail/label_anim.js
@@ -2,9 +2,14 @@ let divs = document.querySelectorAll("form .text-input");
 let domain = document.querySelector("#username > span");
 divs.forEach(d => {
     d.firstElementChild.addEventListener("focus", _ => {
-        d.children[1].classList.add("top");
+        d.children[1].classList.add("top", "active");
     })
     d.firstElementChild.addEventListener("blur", _ => {
-        d.children[1].classList.remove("top");
+        d.children[1].classList.add("hack");
+        d.children[1].classList.remove("active");
+        if (d.children[0].value === "") {
+            d.children[1].classList.remove("hack");
+            d.children[1].classList.remove("top");
+        }
     })
 })
