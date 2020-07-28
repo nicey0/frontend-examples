be15f00d7997bd0fb3c0e8301ff0a06164d0b0e1
nicey0 <nicey_pm@protonmail.com>
2020-07-28 12:45:38 -0300

    epic animations

--- Gmail/label_anim.js
+++ Gmail/label_anim.js
@@ -1,7 +1,10 @@
 let divs = document.querySelectorAll("form .text-input");
 let domain = document.querySelector("#username > span");
 divs.forEach(d => {
-    d.firstElementChild.addEventListener("focus", e => {
-        console.log(e);
+    d.firstElementChild.addEventListener("focus", _ => {
+        d.children[1].classList.add("top");
+    })
+    d.firstElementChild.addEventListener("blur", _ => {
+        d.children[1].classList.remove("top");
     })
 })
