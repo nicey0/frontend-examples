53a58aafd889ff82373fe94d242fce2f1a0b404e
nicey0 <nicey_pm@protonmail.com>
2020-07-28 13:17:32 -0300

    domain name now lights up! :D

--- Gmail/label_anim.js
+++ Gmail/label_anim.js
@@ -1,15 +1,23 @@
+// Domain fix
+let container = document.getElementById("username");
+let input = container.getElementsByClassName("text-input")[0].children[0];
+let domain = container.getElementsByTagName("span")[1];
+// Label animation
 let divs = document.querySelectorAll("form .text-input");
-let domain = document.querySelector("#username > span");
 divs.forEach(d => {
     d.firstElementChild.addEventListener("focus", _ => {
         d.children[1].classList.add("top", "active");
+        if (d.firstElementChild === input)
+            domain.classList.add("domain-active");
     })
     d.firstElementChild.addEventListener("blur", _ => {
         d.children[1].classList.add("hack");
         d.children[1].classList.remove("active");
-        if (d.children[0].value === "") {
+        if (d.firstElementChild.value === "") {
             d.children[1].classList.remove("hack");
             d.children[1].classList.remove("top");
         }
+        if (d.firstElementChild === input)
+            domain.classList.remove("domain-active");
     })
-})
+});
