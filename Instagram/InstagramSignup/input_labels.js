946d1884ea05c7c4f1afc349bb90617fffc8e4b7
nicey0 <nicey_pm@protonmail.com>
2020-07-29 10:29:21 -0300

    enable button when all inputs are valid

--- Instagram/InstagramSignup/input_labels.js
+++ Instagram/InstagramSignup/input_labels.js
@@ -1,5 +1,8 @@
 divs = document.getElementsByClassName("input_labeled");
-for (let d of divs) {
+signup_btn = document.getElementById("submit")
+form_valid = [false, false, false, false];
+for (let i=0; i<divs.length; i++) {
+    let d = divs[i];
     d.firstElementChild.addEventListener("input", _ => {
         if (d.firstElementChild.value !== "") { // Has input
             d.children[1].classList.add("top");
@@ -9,7 +12,15 @@ for (let d of divs) {
             d.children[1].classList.remove("top");
             d.firstElementChild.classList.remove("bot");
         }
-        displayCorrectVIcon(d.firstElementChild.value, d.querySelector(".v"), d.querySelector(".i"));
+        displayCorrectVIcon(d.firstElementChild.value, d.querySelector(".v"), d.querySelector(".i"), i);
+        if (areEqual(form_valid, [true, true, true, true])) {
+            signup_btn.classList.remove("inactive");
+            signup_btn.disabled = false;
+            console.log("yeet");
+        } else {
+            signup_btn.classList.add("inactive");
+            signup_btn.disabled = true;
+        }
     });
 }
 
@@ -19,18 +30,29 @@ function isValidInput(value) {
     else return null;
 }
 
-function displayCorrectVIcon(value, valid, invalid) {
+function displayCorrectVIcon(value, valid, invalid, findex) {
     switch (isValidInput(value)) {
     case true:
         valid.style.display = "inline-block";
         invalid.style.display = "none";
+        form_valid[findex] = true;
         break;
     case  false:
         valid.style.display = "none";
         invalid.style.display = "inline-block";
+        form_valid[findex] = false;
         break;
     default:
         valid.style.display = "none";
         invalid.style.display = "none";
+        form_valid[findex] = false;
     }
+}
+
+function areEqual(ar1, ar2) {
+    for (let i=0; i<5; i++) {
+        if (ar1[i] !== ar2[i])
+            return false;
+    }
+    return true;
 }
