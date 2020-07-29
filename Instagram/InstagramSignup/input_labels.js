2d61180f17bd872c48a7fc2dc06dc68347d56451
nicey0 <nicey_pm@protonmail.com>
2020-07-29 10:18:47 -0300

    valid/invalid icons now show properly :^)

--- Instagram/InstagramSignup/input_labels.js
+++ Instagram/InstagramSignup/input_labels.js
@@ -1,21 +1,36 @@
-function isValidInput(value) {
-    return value === "valid";
-}
-
 divs = document.getElementsByClassName("input_labeled");
 for (let d of divs) {
     d.firstElementChild.addEventListener("input", _ => {
         if (d.firstElementChild.value !== "") { // Has input
             d.children[1].classList.add("top");
             d.firstElementChild.classList.add("bot");
-            if (isValidInput(d.firstElementChild.value))
-                d.lastElementChild.style.display = "block";
-            else
-                d.lastElementChild.style.display = "none";
         }
         else { // Doesn't have input
             d.children[1].classList.remove("top");
             d.firstElementChild.classList.remove("bot");
         }
+        displayCorrectVIcon(d.firstElementChild.value, d.querySelector(".v"), d.querySelector(".i"));
     });
+}
+
+function isValidInput(value) {
+    if (value === "valid") return true;
+    else if (value === "invalid") return false;
+    else return null;
+}
+
+function displayCorrectVIcon(value, valid, invalid) {
+    switch (isValidInput(value)) {
+    case true:
+        valid.style.display = "inline-block";
+        invalid.style.display = "none";
+        break;
+    case  false:
+        valid.style.display = "none";
+        invalid.style.display = "inline-block";
+        break;
+    default:
+        valid.style.display = "none";
+        invalid.style.display = "none";
+    }
 }
