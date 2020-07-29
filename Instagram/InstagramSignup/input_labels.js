a1c2e401f74903899c88e5e21bf55fbb3045e1a7
nicey0 <nicey_pm@protonmail.com>
2020-07-29 09:44:19 -0300

    input labels done! :D

--- Instagram/InstagramSignup/input_labels.js
+++ Instagram/InstagramSignup/input_labels.js
@@ -1,5 +1,17 @@
 divs = document.getElementsByClassName("input_labeled");
 for (let d of divs) {
-    d.addEventListener(_ => {
+    console.log(d);
+    d.firstElementChild.addEventListener("input", _ => {
+        if (d.firstElementChild.value !== "") {
+            d.lastElementChild.classList.add("top");
+            d.firstElementChild.classList.add("bot");
+        }
+        else {
+            d.lastElementChild.classList.remove("top");
+            d.firstElementChild.classList.remove("bot");
+        }
+    })
+    d.firstElementChild.addEventListener("blur", _ => {
+        console.log(d.firstElementChild.value);
     })
 }
