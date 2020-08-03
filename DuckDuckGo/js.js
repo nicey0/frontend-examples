6403f397370b0c654e10db31f70d142997b40d34
nicey0 <nicey_pm@protonmail.com>
2020-08-03 20:27:33 -0300

    links section

--- DuckDuckGo/js.js
+++ DuckDuckGo/js.js
@@ -1,3 +1,4 @@
+/* Sidebar */
 sidebar = document.getElementsByTagName("aside")[0];
 open = document.getElementsByClassName("sidebar-btn")[0];
 close = sidebar.children['x'];
@@ -12,4 +13,18 @@ close.addEventListener("click", _ => {
 
 function toggleSidebar(new_state) {
     new_state ? sidebar.classList.add("active") : sidebar.classList.remove("active");
+}
+
+/* Social Media Links */
+button = document.getElementsByClassName("links")[0];
+links = document.getElementById("sm-links");
+open = false;
+
+button.addEventListener("click", _ => {
+    open = !open;
+    toggle_links_sect(open);
+})
+
+function toggle_links_sect(new_state) {
+    new_state ? links.classList.add("active") : links.classList.remove("active");
 }
